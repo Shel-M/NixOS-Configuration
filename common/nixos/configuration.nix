d6b749945d6a9397c6a654593b9253ab13a1847c
Sheldon M. <blackshadow606@gmail.com>
2025-02-11 19:25:26 -0700

    Adding fish and wl-clipboard

--- common/nixos/configuration.nix
+++ common/nixos/configuration.nix
@@ -53,6 +53,7 @@
     packages = with pkgs; [
       firefox
       discord
+      wl-clipboard
       zig
     ];
   };
@@ -83,6 +84,15 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
+  programs.bash = {
+  interactiveShellInit = ''
+    if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+    then
+      shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+      exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+    fi
+    '';
+  };
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
